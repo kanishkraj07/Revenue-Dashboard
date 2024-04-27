e0913645c77a4b137e8b92b66d5febb113f6f408
kanishkraj07 <kanishkmogalraj@gmail.com>
2024-04-28 00:46:07 +0530

    added few css changes

--- payouts-dashboard/src/components/Overview.jsx
+++ payouts-dashboard/src/components/Overview.jsx
@@ -1,12 +1,12 @@
 import { PayoutCard } from "./PayoutCard";
 
 export function Overview() {
-    return <div className="bg-[#0c4a6e] p-4 flex flex-col gap-6">
+    return <div className="bg-[#0c4a6e] p-4 flex flex-col gap-6 mt-5">
         <div className="text-cream-300 text-2xl font-sans">Overview</div>
         <div className='grid grid-cols-1 gap-y-6 md:grid-cols-2 gap-x-6 xl:grid-cols-3'>
-            <PayoutCard varient={"primary"} title={"Amount Pending"} amount={"95,312.30"} ordersCount={15} footerTitle={"Next Payment Date"} footerVal={"Today, 4:00PM"}></PayoutCard>
-            <PayoutCard title={"Amount Pending"} amount={"83,312.23"} ordersCount={15}></PayoutCard>
-            <PayoutCard title={"Amount Pending"} amount={"65,312.50"} ordersCount={15}></PayoutCard>
+            <PayoutCard varient={"primary"} title={"Next Payout"} amount={"12,312.30"} ordersCount={5} footerTitle={"Next Payment Date"} footerVal={"Today, 4:00PM"}></PayoutCard>
+            <PayoutCard title={"Amount Pending"} amount={"23,312.23"} ordersCount={12}></PayoutCard>
+            <PayoutCard title={"Amount Processed"} amount={"2,65,312.50"} ordersCount={54}></PayoutCard>
         </div>
     </div>
 }
--- payouts-dashboard/src/components/PayoutHeader.jsx
+++ payouts-dashboard/src/components/PayoutHeader.jsx
@@ -1,7 +1,7 @@
 export function PayoutHeader() {
-    return <div className="inline-flex justify-between bg-white p-5 md:flex">
+    return <div className="inline-flex justify-between bg-white p-5 md:flex border-b border-b-solid border-b-[rgba(0,0,0,0.2)]">
         <div className="flex justify-center items-center gap-3 ">
-            <div className="text-black text-lg sm:text-2xl">Instant Pay</div>
+            <div className="text-black text-lg sm:text-2xl">InstantPay</div>
             <div className="flex justify-center items-center gap-2 text-gray-500">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
@@ -10,26 +10,26 @@ export function PayoutHeader() {
             </div>
         </div>
 
-        <div className="w-15 mx-5 sm:w-30 md:w-50 lg:w-1/3 flex justify-center items-center gap-4 p-3 bg-[#f2f2f2] rounded-lg">
+        <div className="w-15 mx-5 sm:w-30 md:w-50 lg:w-1/3 flex justify-center items-center gap-4 px-3 py-2 bg-[#f2f2f2] rounded-lg">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                     </svg>
-            <input className="w-full outline-0 bg-transparent text-lg" placeholder="Search features, tutorials, etc ..."></input>
+            <input className="w-full outline-0 bg-transparent text-base" placeholder="Search features, tutorials, etc ..."></input>
         </div>
 
         <div className="flex justify-center align-center gap-3">
-            <div className="w-12 h-12 rounded-full bg-[#f2f2f2] flex justify-center items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-8 h-8">
+            <div className="w-10 h-10 rounded-full bg-[#f2f2f2] flex justify-center items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
                 </svg>
             </div>
-            <div className="w-12 h-12 rounded-full bg-[#f2f2f2] flex justify-center items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-8 h-8">
+            <div className="w-10 h-10 rounded-full bg-[#f2f2f2] flex justify-center items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
             </svg>
             </div>
-            <div className="w-12 h-12 rounded-full bg-[#f2f2f2] flex justify-center items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-8 h-8">
+            <div className="w-10 h-10 rounded-full bg-[#f2f2f2] flex justify-center items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
                 <path strokeLinecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
             </svg>
 
